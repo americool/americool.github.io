fc7334e580aed2220a8710d252c1f927394abc98
Abe Anderson <aberanderson@gmail.com>
2018-10-09 22:38:03 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">This is a Super Boring Placeholder for a Real Portfolio</h1>
+        </header>
+        <p className="App-intro">
+          In the mean time you can view my resume <a href="https://resume.creddle.io/resume/i4d7un7f8op">here</a>.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
