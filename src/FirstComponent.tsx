b023d510d76276c15d3ee6244ea0c8dc105c6887
Abe Anderson <aberanderson@gmail.com>
2018-10-10 23:41:15 -0400

    ejected and redux working

--- src/FirstComponent.tsx
+++ src/FirstComponent.tsx
@@ -1,16 +1,42 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { IRootState } from './store';
+import { switchTest } from './store/main/actions';
 
 import Button from '@material-ui/core/Button';
 
+interface IProps extends IActions {
+  test: boolean;
+}
 
-export default class FirstComponent extends React.Component<{},{}> {
+interface IActions {
+  switchTest(value: boolean): void;
+}
+
+export class FirstComponent extends React.Component<IProps,{}> {
   public render() {
     return (
-      <Button onClick={this.handleClick}>Click Me</Button>
+      <div>
+        <Button onClick={this.handleClick}>Click Me</Button>
+        <div>Current Value is {this.props.test.toString()}</div>
+      </div>
     )
   }
 
   private handleClick = () => {
-    console.log('yo');
+    this.props.switchTest(!this.props.test);
   }
 }
+
+export const mapStateToProps = (state: IRootState) => {
+  const { main: { test } } = state;
+  return {
+    test
+  }
+}
+
+const mapDispatchToProps: IActions = {
+  switchTest
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FirstComponent)
